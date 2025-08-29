6c711b1a00f5f0482645190498ae5d223d6b94fc
Grant Geist <granthgeist@gmail.com>
2025-08-29 11:15:01 +0000

    fix port

--- app/run.py
+++ app/run.py
@@ -233,7 +233,7 @@ def main():
     Returns:
     None
     """
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=3000, debug=False)
 
 if __name__ == '__main__':
     main()
